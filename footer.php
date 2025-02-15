3718df113252ec107199a46ae8a7b8d08501f310
natalycux <ncuxr@miumg.edu.gt>
2025-02-15 07:20:32 -0600

    xreate: add footer

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="footer">
+    <div class="logo">
+        <img src="/public/assets/images/logo.png" alt="Logo">
+    </div>
+    <div class="socials">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+    </div>
+    <div class="links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+    </div>
+    <div class="copyright">
+        &copy; <?php echo date("Y"); ?> Your Company. All rights reserved.
+    </div>
+    <div class="back-to-top" onclick="window.scrollTo({ top: 0, behavior: 'smooth' });">
+        &#8679;
+    </div>
+</footer>
